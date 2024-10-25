9d6aa031d4e06f6cf9f7d38c0ebec4dc4ba43f38
Soloma <Soloma3040450@Gmail.com>
2024-10-25 19:41:15 +0300

    Переход на новый фреймворк .net Framework

--- Library/Vertex.cs
+++ Library/Vertex.cs
@@ -3,19 +3,28 @@ namespace Library
     /// <summary>
     /// класс, представляющий вершину из X и Y координат.
     /// </summary>
-    /// <param name="xCord">X-координата вершины.</param>
-    /// <param name="yCord">Y-координата вершины.</param>
-    public class Vertex(int xCord, int yCord)
+    public class Vertex
     {
         /// <summary>
         /// X-координата вершины.
         /// </summary>
-        public int xCord = xCord;
+        public int xCord;
         /// <summary>
         /// Y-координата вершины.
         /// </summary>
-        public int yCord = yCord;
+        public int yCord;
 
+        /// <summary>
+        /// Конструктор класса Vertex.
+        /// </summary>
+        /// <param name="xCord">X-координата вершины.</param>
+        /// <param name="yCord">Y-координата вершины.</param>
+        public Vertex(int xCord, int yCord)
+        {
+            this.xCord = xCord;
+            this.yCord = yCord;
+        }
+        
         /// <summary>
         /// вывод информации о вершинаъ.
         /// </summary>
